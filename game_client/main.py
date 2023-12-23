23843905d4bf26f6b02c8ed2d30d0856dc90ef25
Wen Chun Tang <dig84128@gmail.com>
2023-12-23 19:24:44 +0800

    Revert "update"
    This reverts commit 0ba53665e29041cb5269590113f5be9b21e2ed51.

--- game_client/main.py
+++ game_client/main.py
@@ -14,10 +14,121 @@ import math
 
 text = None
 should_continue = True
+video_start = False
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
+# 根據兩點的座標，計算角度
+def vector_2d_angle(v1, v2):
+    v1_x = v1[0]
+    v1_y = v1[1]
+    v2_x = v2[0]
+    v2_y = v2[1]
+    try:
+        angle_= math.degrees(math.acos((v1_x*v2_x+v1_y*v2_y)/(((v1_x**2+v1_y**2)**0.5)*((v2_x**2+v2_y**2)**0.5))))
+    except:
+        angle_ = 180
+    return angle_
+
+# 根據傳入的 21 個節點座標，得到該手指的角度
+def hand_angle(hand_):
+    angle_list = []
+    # thumb 大拇指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[2][0])),(int(hand_[0][1])-int(hand_[2][1]))),
+        ((int(hand_[3][0])- int(hand_[4][0])),(int(hand_[3][1])- int(hand_[4][1])))
+        )
+    angle_list.append(angle_)
+    # index 食指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])-int(hand_[6][0])),(int(hand_[0][1])- int(hand_[6][1]))),
+        ((int(hand_[7][0])- int(hand_[8][0])),(int(hand_[7][1])- int(hand_[8][1])))
+        )
+    angle_list.append(angle_)
+    # middle 中指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[10][0])),(int(hand_[0][1])- int(hand_[10][1]))),
+        ((int(hand_[11][0])- int(hand_[12][0])),(int(hand_[11][1])- int(hand_[12][1])))
+        )
+    angle_list.append(angle_)
+    # ring 無名指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[14][0])),(int(hand_[0][1])- int(hand_[14][1]))),
+        ((int(hand_[15][0])- int(hand_[16][0])),(int(hand_[15][1])- int(hand_[16][1])))
+        )
+    angle_list.append(angle_)
+    # pink 小拇指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[18][0])),(int(hand_[0][1])- int(hand_[18][1]))),
+        ((int(hand_[19][0])- int(hand_[20][0])),(int(hand_[19][1])- int(hand_[20][1])))
+        )
+    angle_list.append(angle_)
+    return angle_list
+
+# 根據手指角度的串列內容，返回對應的手勢名稱
+def hand_pos(finger_angle):
+    f1 = finger_angle[0]   # 大拇指角度
+    f2 = finger_angle[1]   # 食指角度
+    f3 = finger_angle[2]   # 中指角度
+    f4 = finger_angle[3]   # 無名指角度
+    f5 = finger_angle[4]   # 小拇指角度
+
+    # 小於 50 表示手指伸直，大於等於 50 表示手指捲縮
+    if f1<50 and f2>=50 and f3>=50 and f4>=50 and f5>=50:
+        return 'launch'
+    elif f1>=50 and f2<50 and f3>=50 and f4>=50 and f5>=50:
+        return 'right'
+    elif f1>=50 and f2<50 and f3<50 and f4>=50 and f5>=50:
+        return 'left'
+    elif f1<50 and f2<50 and f3<50 and f4<50 and f5<50:
+        return 'Stop'
+    else:
+        return ''
+    
+def hand_recognition():
+    global text, video_start
+    cap = cv2.VideoCapture(0)
+    fontFace = cv2.FONT_HERSHEY_SIMPLEX
+    lineType = cv2.LINE_AA
+    
+    with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+
+        if not cap.isOpened():
+            print("Cannot open camera")
+            exit()
+        w, h = 540, 310
+        while True:
+            ret, img = cap.read()
+            img = cv2.resize(img, (w, h))
+            if not ret:
+                print("Cannot receive frame")
+                break
+            video_start = True
+
+            img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = hands.process(img2)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    finger_points = []
+                    for i in hand_landmarks.landmark:
+                        x = i.x * w
+                        y = i.y * h
+                        finger_points.append((x, y))
+                    if finger_points:
+                        finger_angle = hand_angle(finger_points)
+                        answer = hand_pos(finger_angle)
+                        text = answer
+                        cv2.putText(img, answer, (30, 120), fontFace, 5, (255, 255, 255), 10, lineType)
+            cv2.imshow('oxxostudio', img)
+            if cv2.waitKey(5) == ord('q'):
+                break
+    cap.release()
+    cv2.destroyAllWindows()
 
 def voice_translate(r,audio,stopwords,coding_syntax,learning_environment,project_assignment):
     global text
@@ -139,64 +250,14 @@ def voice_recognition():
                 clear_counter = 0
                 os.system('clear')
 
-
-
-def hand_recognition():
-    global text
-    import cv2
-    import mediapipe as mp
-    import math
-    cap = cv2.VideoCapture(0)            # 讀取攝影機
-    fontFace = cv2.FONT_HERSHEY_SIMPLEX  # 印出文字的字型
-    lineType = cv2.LINE_AA               # 印出文字的邊框
-
-    # mediapipe 啟用偵測手掌
-    with mp_hands.Hands(
-        model_complexity=0,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
-
-        if not cap.isOpened():
-            print("Cannot open camera")
-            exit()
-        w, h = 540, 310                                  # 影像尺寸
-        while True:
-            ret, img = cap.read()
-            img = cv2.resize(img, (w,h))                 # 縮小尺寸，加快處理效率
-            if not ret:
-                print("Cannot receive frame")
-                break
-            [img2] = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 轉換成 RGB 色彩
-            results = hands.process(img2)                # 偵測手勢
-
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    finger_points = []                   # 記錄手指節點座標的串列
-                    for i in hand_landmarks.landmark:
-                        thumb_tip_x = i[mp.solutions.hands.HandLandmark.THUMB_TIP].x
-                        # 將 21 個節點換算成座標，記錄到 finger_points
-                        x = i.x*w
-                        y = i.y*h
-                        finger_points.append((x,y))
-                    if finger_points:
-                        finger_angle = hand_angle(finger_points) # 計算手指角度，回傳長度為 5 的串列
-                        #print(finger_angle)                     # 印出角度 ( 有需要就開啟註解 )
-                        text = hand_pos(finger_angle)            # 取得手勢所回傳的內容
-                        cv2.putText(img, text, (30,120), fontFace, 5, (255,255,255), 10, lineType) # 印出文字
-
-            cv2.imshow('oxxostudio', img)
-            if cv2.waitKey(5) == ord('q'):
-                break
-    cap.release()
-    cv2.destroyAllWindows()
 #############################################
 
 
 # 启动语音识别线程
 threading.Thread(target=voice_recognition, daemon=True).start()
 
-# # 启动语音识别线程
-# threading.Thread(target=hand_recognition, daemon=True).start()
+# # 启动手部识别线程
+threading.Thread(target=hand_recognition, daemon=True).start()
 
 WIDTH = 640
 HEIGHT = 480
@@ -256,17 +317,18 @@ merry_christmas_img = pygame.transform.scale(merry_christmas_img, (WIDTH, HEIGHT
 
 game_over_img = pygame.image.load(os.path.join(img_folder, '聖誕快樂gameover圖.png')).convert()
 game_over_img = pygame.transform.scale(game_over_img, (WIDTH, HEIGHT))  # 調整圖片大小以適應螢幕
+while not video_start:
+    continue
 
 while game_run:
-    if text =='左邊'or text =='左左左' or text =='左左左左':
+    if text =='左邊'or text =='左左左' or text =='左左左左'or text == 'left':
         player.move_left()
-    if text =='右邊'or text =='右'or text =='右右右右'or text =='右右右':
+    if text =='右邊'or text =='右'or text =='右右右右'or text =='右右右'or text == 'right':
         player.move_right()
-    if text =='發射' or text =='發' or text =='射'or text =='發射發射' :
+    if text =='發射' or text =='發' or text =='射'or text =='發射發射' or text == 'launch':
         text = ''
         position = player.get_position()
-        for i in range(10):   
-            MISSILES.append(Missile(position[0], position[1]+i*10))
+        MISSILES.append(Missile(position[0], position[1]))
     if enemy.health <= 0:
         while True:
             screen.blit(merry_christmas_img, (0, 0))  # 顯示遊戲結束的圖片
